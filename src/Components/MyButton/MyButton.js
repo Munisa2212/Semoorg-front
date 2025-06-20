1aff9e2f2bcae94db5a2febb78e6fe928df944bb
Anvar <61209846+AnvarIbodullayev@users.noreply.github.com>
2025-06-20 21:23:34 +0100

    Update MyButton.js

--- src/Components/MyButton/MyButton.js
+++ src/Components/MyButton/MyButton.js
@@ -2,8 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledBtn = styled.button`
-  background: ${({ rangi }) => rangi || "#fff"};
-  color: ${({ variant }) => (variant === "dark" ? "#fff" : "#000")};
+  background: ${({ $rangi }) => $rangi || "#fff"};
+  color: ${({ $variant }) => ($variant === "dark" ? "#fff" : "#000")};
   border: 1px solid transparent;
   padding: 12px 20px;
   font-weight: 600;
@@ -11,24 +11,31 @@ const StyledBtn = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  svg{
+  svg {
     margin-right: 10px;
   }
   &:hover {
-    border: 1px solid ${({ rangi }) => rangi || "#000"};
+    border: 1px solid ${({ $rangi }) => $rangi || "#000"};
   }
   &:active {
     transform: scale(0.95);
   }
 `;
 
-function MyButton({ onSubmit, icon, className, title, variant, rangi }) {
+function MyButton({ onClick, onSubmit, icon, className, title, variant, rangi }) {
   return (
-    <StyledBtn onSubmit={onSubmit} className={className} variant={variant} rangi={rangi}>
+    <StyledBtn
+      onClick={onClick}
+      onSubmit={onSubmit}
+      className={className}
+      $variant={variant}
+      $rangi={rangi}
+    >
       {icon}
       <span>{title}</span>
     </StyledBtn>
   );
 }
+
 
 export default MyButton;

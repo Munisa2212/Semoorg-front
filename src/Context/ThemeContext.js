697b6893e9dcc55666d431a40e3253a3440cc22a
Anvar <61209846+AnvarIbodullayev@users.noreply.github.com>
2025-06-20 21:23:53 +0100

    Update ThemeContext.js

--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -2,6 +2,8 @@ import React from "react";
 const ThemeContext = React.createContext({
     theme: "light",
     toggleTheme: () => {},
+    initialScreen: true,
+    setInitialScreen: () => {},
 });
 
 export default ThemeContext;
